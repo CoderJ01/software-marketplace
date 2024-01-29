6eb9e4a9b2c342f27f3594f361d2c094998be128
CoderJ01 <buildingguy@sbcglobal.net>
2024-01-28 20:12:11 -0800

    create Header.util.js to 'separate concerns'

---
+++ client/src/Components/Header/Header.util.js
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
+
+export const drawerWidth = 240;
+export const navItems = ['Home', 'Login', 'Sign Up'];
+
+export function setDrawer(handleDrawerToggle) {
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+          <Typography variant="h6" sx={{ my: 2 }}>
+            MUI
+          </Typography>
+          <Divider />
+          <List>
+            {navItems.map((item) => (
+              <ListItem key={item} disablePadding>
+                <ListItemButton sx={{ textAlign: 'center' }}>
+                  <ListItemText primary={item} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+    );
+    return drawer;
+}
+
+export function setContainer(window) {
+    const container = window !== undefined ? () => window().document.body : undefined;
+    return container;
+}
