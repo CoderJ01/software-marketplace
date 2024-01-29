7dc9880f8811b8bf07847e2237b61d0a46812fbb
CoderJ01 <buildingguy@sbcglobal.net>
2024-01-28 20:12:43 -0800

    adjust coressponding jsx accordingly

--- client/src/Components/Header/Header.component.jsx
+++ client/src/Components/Header/Header.component.jsx
@@ -3,20 +3,14 @@ import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
-const drawerWidth = 240;
-const navItems = ['Home', 'Login', 'Sign Up'];
+import { drawerWidth, setDrawer, setContainer, navItems } from './Header.util';
 
 function Header(props) {
   const { window } = props;
@@ -25,26 +19,6 @@ function Header(props) {
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
-  const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -78,7 +52,7 @@ function Header(props) {
       </AppBar>
       <nav>
         <Drawer
-          container={container}
+          container={setContainer(window)}
           variant="temporary"
           open={mobileOpen}
           onClose={handleDrawerToggle}
@@ -90,7 +64,7 @@ function Header(props) {
             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
           }}
         >
-          {drawer}
+          {setDrawer(handleDrawerToggle)}
         </Drawer>
       </nav>
       <Box component="main" sx={{ p: 3 }}>
