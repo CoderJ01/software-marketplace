505d882848a2c2861f405ef2903409b8afaf4535
CoderJ01 <buildingguy@sbcglobal.net>
2024-01-28 19:19:06 -0800

    change function name

--- client/src/Components/Header.component.jsx
+++ client/src/Components/Header.component.jsx
@@ -134,7 +134,7 @@ function Header(props) {
   );
 }
 
-DrawerAppBar.propTypes = {
+Header.propTypes = {
   /**
    * Injected by the documentation to work in an iframe.
    * You won't need it on your project.
