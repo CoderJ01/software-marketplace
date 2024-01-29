fe1ffc4329f6113b492e7ddebff82d65652dec4b
CoderJ01 <buildingguy@sbcglobal.net>
2024-01-28 19:09:31 -0800

    import Header to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+// components
+import Header from './Components/Header.component';
+
 function App() {
   return (
     <div className='App'>
-      
+      <Header/>
     </div>
   );
 }
