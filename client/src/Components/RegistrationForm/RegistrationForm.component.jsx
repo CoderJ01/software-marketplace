6f70d7fd9ea0313b49b45d1b53447475093edd92
CoderJ01 <buildingguy@sbcglobal.net>
2024-02-01 18:45:33 -0800

    adjust jsx

--- client/src/Components/RegistrationForm/RegistrationForm.component.jsx
+++ client/src/Components/RegistrationForm/RegistrationForm.component.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import {
   Grid,
-  makeStyles,
   Card,
   CardContent,
   MenuItem,
@@ -14,67 +13,9 @@ import {
 } from "@material-ui/core"
 
 import { Formik, Form, Field } from "formik"
-import * as Yup from "yup"
 import { TextField } from "formik-material-ui"
 
-const useStyle = makeStyles((theme) => ({
-  padding: {
-    padding: theme.spacing(3),
-  },
-  button: {
-    margin: theme.spacing(1),
-  },
-  margin: {
-    marginTop: '15vh',
-  }
-}))
-
-//Data
-const initialValues = {
-  firstName: "",
-  lastName: "",
-  occupation: "",
-  city: "",
-  country: "",
-  email: "",
-  password: "",
-}
-
-const options = [
-  { label: "Automotive", value: "automotive" },
-  { label: "Beauty", value: "beauty" },
-  { label: "Education", value: "education" },
-  { label: "Healthcare", value: "health_care" },
-  { label: "Information Technology", value: "information_technology" },
-  { label: "Legal", value: "legal" },
-  { label: "Restaurant", value: "restaurant" },
-  { label: "Retail", value: "retail" },
-]
-
-//password validation
-const lowercaseRegEx = /(?=.*[a-z])/
-const uppercaseRegEx = /(?=.*[A-Z])/
-const numericRegEx = /(?=.*[0-9])/
-const lengthRegEx = /(?=.{6,})/
-
-//validation schema
-let validationSchema = Yup.object().shape({
-  firstName: Yup.string().required("Required"),
-  lastName: Yup.string().required("Required"),
-  email: Yup.string().email("Invalid email").required("Required"),
-  password: Yup.string()
-    .matches(
-      lowercaseRegEx,
-      "Must contain one lowercase alphabetical character!"
-    )
-    .matches(
-      uppercaseRegEx,
-      "Must contain one uppercase alphabetical character!"
-    )
-    .matches(numericRegEx, "Must contain one numeric character!")
-    .matches(lengthRegEx, "Must contain 6 characters!")
-    .required("Required!"),
-})
+import { useStyle, initialValues, options, validationSchema } from "./RegistrationForm.util"
 
 const RegistrationForm = () => {
   const classes = useStyle()
