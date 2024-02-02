08cbbd09fbd9732448b84c86f8dbdb4e4aa3cf3a
CoderJ01 <buildingguy@sbcglobal.net>
2024-02-01 17:52:08 -0800

    import

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 // components
 import Header from './Components/Header/Header.component';
+import RegistrationForm from './Components/RegistrationForm/RegistrationForm.component';
 
 function App() {
   return (
     <div className='App'>
       <Header/>
+      <RegistrationForm/>
     </div>
   );
 }
