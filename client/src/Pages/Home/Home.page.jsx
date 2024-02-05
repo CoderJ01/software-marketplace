d69c1e26af56a9304d7d240a8dc5c8579f404d89
CoderJ01 <buildingguy@sbcglobal.net>
2024-02-05 15:07:51 -0800

    import

--- client/src/Pages/Home/Home.page.jsx
+++ client/src/Pages/Home/Home.page.jsx
@@ -5,33 +5,38 @@ import Typography from '@mui/material/Typography';
 import neonKeyboard from '../../assets/images/neon-keyboard.jpg'
 import Button from '@mui/material/Button';
 import './Home.style.css';
+import { navigateToPage } from './Home.util';
+
+import { useNavigate } from 'react-router-dom';
 
 function Homepage() {
-  return (
-    <>
-        <img className={'neon-keyboard'} style={{ height: '60vh', width: '100%'}} src={neonKeyboard}></img>
-        <Container 
-            style={{ 
-                display: 'flex', 
-                justifyContent: 'center',
-                paddingBottom: '5vh',
-                paddingTop: '5vh' 
-            }} 
-            maxWidth='lg'
-        >
-            <Box sx={{ width: '100%', maxWidth: 500 }}>
-                <Typography className={"heading"} variant="h6" gutterBottom style={{ display: 'flex', justifyContent:'center' }}>
-                    A Market for Software
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                    Greetings! Are you looking for a specific piece of software for your website, be it a form or table?
-                    If so, you've come to the right place. Search this site for a variety of tools you can use for your website.
-                </Typography>
-                <Button variant="contained" style={{ marginTop: '2vh', marginLeft: '35%', width: '30%' }}>See Software</Button>
-            </Box>
-        </Container>
-    </>
-  );
+    let navigate = useNavigate();
+
+    return (
+        <>
+            <img className={'neon-keyboard'} style={{ height: '60vh', width: '100%'}} src={neonKeyboard}></img>
+            <Container 
+                style={{ 
+                    display: 'flex', 
+                    justifyContent: 'center',
+                    paddingBottom: '5vh',
+                    paddingTop: '5vh' 
+                }} 
+                maxWidth='lg'
+            >
+                <Box sx={{ width: '100%', maxWidth: 500 }}>
+                    <Typography className={"heading"} variant="h6" gutterBottom style={{ display: 'flex', justifyContent:'center' }}>
+                        A Market for Software
+                    </Typography>
+                    <Typography variant="body1" gutterBottom>
+                        Greetings! Are you looking for a specific piece of software for your website, be it a form or table?
+                        If so, you've come to the right place. Search this site for a variety of tools you can use for your website.
+                    </Typography>
+                    <Button variant="contained" style={{ marginTop: '2vh', marginLeft: '35%', width: '30%' }} onClick={() => navigateToPage(navigate)}>See Software</Button>
+                </Box>
+            </Container>
+        </>
+    );
 }
 
 export default Homepage
