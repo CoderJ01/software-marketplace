b35281486ec66592ae5da4e857836df690a2b2bc
CoderJ01 <buildingguy@sbcglobal.net>
2024-02-05 13:36:19 -0800

    set up client routes for pages and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
+// React
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// CSS
 import './App.css';
+
+// Pages
+import Homepage from './Pages/Home/Home.page';
+import Software from './Pages/Software/Software.page';
 
 // components
 import Header from './Components/Header/Header.component';
-// import RegistrationForm from './Components/RegistrationForm/RegistrationForm.component';
-// import LoginForm from './Components/LoginForm/LoginForm.component';
-import Homepage from './Pages/Home/Home.page';
-import Software from './Pages/Software/Software.page';
+import RegistrationForm from './Components/RegistrationForm/RegistrationForm.component';
+import LoginForm from './Components/LoginForm/LoginForm.component';
 
 function App() {
   return (
     <div className='App'>
       <Header/>
-      {/* <RegistrationForm/> */}
-      {/* <LoginForm/> */}
-      {/* <Homepage/> */}
-      <Software/>
+       <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Homepage/>}/>
+                <Route path='/software' element={<Software/>}/>
+                <Route path='/sign-up' element={<RegistrationForm/>}/>
+                <Route path='/login' element={<LoginForm/>}/>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
