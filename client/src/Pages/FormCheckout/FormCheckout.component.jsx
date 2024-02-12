c0f9e2d5919f88fa349dd456b4e813aab623c40a
CoderJ01 <buildingguy@sbcglobal.net>
2024-02-11 21:01:59 -0800

    edit jsx

--- client/src/Pages/FormCheckout/FormCheckout.component.jsx
+++ client/src/Pages/FormCheckout/FormCheckout.component.jsx
@@ -1,24 +1,14 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Unstable_Grid2';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import { TextField, Button, Typography } from "@mui/material";
 import MenuItem from '@mui/material/MenuItem';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox'
+import Checkbox from '@mui/material/Checkbox';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
-
-const number = ['1', '2', '3'];  
+import { Item, number, processSubmission, trackCheckmarks } from './FormCheckout.util';
 
 export default function FormCheckout() {
     const [formData, setFormData] = useState({ 
@@ -42,57 +32,11 @@ export default function FormCheckout() {
 
     function handleSubmit(e) {
         e.preventDefault();
-
-        if(formData.formName === '') {
-            setErrorMessage('The form needs a name!');
-            return;
-        }
-
-        if(
-            formData.firstName === false &&
-            formData.lastName === false &&
-            formData.email === false &&
-            formData.username === false &&
-            formData.password === false &&
-            formData.city === false &&
-            formData.state === false &&
-            formData.phoneNumber === false &&
-            formData.other === false
-        )
-        {
-            setErrorMessage('The form must have at least one field!');
-            return;
-        }
-
-        if(formData.other === true && formData.extraFields === '') {
-            setErrorMessage('Additional fields are required. If you want no additional fields, uncheck \'Other\'!');
-            return;
-        }
-
-        if(formData.numberOfForms === 0) {
-            setErrorMessage('You must select the number of forms you want!');
-            return;
-        }
-
-        setErrorMessage('');
-        let fieldObj = formData.extraFields.split(',');
-        for(let i = 0; i < fieldObj.length; i++) {
-            if(fieldObj[i] != '') {
-                setFields([...fields, fieldObj[i]]);
-            }
-        }
-        fieldObj = [];
-        console.log(fields);
+        processSubmission(formData, setErrorMessage, setFields, fields);
     }
 
     function handleCheckmark(e) {
-        if(formData[e.target.name] === false) {
-            setFormData({ ...formData, [e.target.name]: true });
-            setFields([...fields, e.target.name.toString()]);
-        }
-        else if(formData[e.target.name] === true) {
-            setFormData({ ...formData, [e.target.name]: false });
-        }
+        trackCheckmarks(e, formData, setFormData, setFields, fields)
     }
 
     return (
