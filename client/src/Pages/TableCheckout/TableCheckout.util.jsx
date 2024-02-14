563c085cdc9520c6c6d7e208cb39c3f1319f4ad8
CoderJ01 <buildingguy@sbcglobal.net>
2024-02-13 21:27:45 -0800

    input validation for table

--- client/src/Pages/TableCheckout/TableCheckout.util.jsx
+++ client/src/Pages/TableCheckout/TableCheckout.util.jsx
@@ -1,3 +1,18 @@
 export function processSubmission(formData, setErrorMessage) {
-    
+    if(formData.tableName === '') {
+        setErrorMessage('Table must have a name!');
+        return;
+    }
+
+    if(formData.columns === '') {
+        setErrorMessage('Table must have at least one column!');
+        return;
+    }
+
+    if(formData.rows === '') {
+        setErrorMessage('Table must have at least one row!');
+        return;
+    }
+
+    setErrorMessage('');
 }
