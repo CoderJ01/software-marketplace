153386e4a8579f53f38a21e499d989a9379c57d6
CoderJ01 <buildingguy@sbcglobal.net>
2024-02-14 18:41:27 -0800

    create Mongoose schema (model) for User

---
+++ server/config/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+    {
+        userID: {
+            type: mongoose.Schema.Types.UUID,
+            required: true,
+            default: uuidv4()
+        },
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        industry: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        state: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: String
+        },
+        token: {
+            type: String
+        }
+    }
+);
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
