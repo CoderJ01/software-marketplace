859d9cb55546e506b1979f6bb3088cf3067883b6
CoderJ01 <buildingguy@sbcglobal.net>
2024-02-14 17:13:01 -0800

    confirm functionality of database connection config

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const colors = require('colors');
 require('dotenv').config();
 const cors = require('cors');
 const PORT = process.env.PORT || 4000;
+const connectDB = require('./config/connection.js');
 
 const app = express();
+
+connectDB();
 
 app.use(cors()); // relax security applied to an API
 app.use('/', (req, res) => {
