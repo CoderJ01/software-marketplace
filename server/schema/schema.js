e10848a0ce0e3a0ab439c8e84bec62517849775c
CoderJ01 <buildingguy@sbcglobal.net>
2024-02-22 21:07:07 -0800

    add resolver

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,3 +2,20 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 const typeDefs = ``;
+
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+];
+
+const resolvers = {
+    Query: {
+      books: () => books,
+    },
+};
