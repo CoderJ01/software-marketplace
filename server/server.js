af239076cc3363b66e3e03b5b8fa891410047692
CoderJ01 <buildingguy@sbcglobal.net>
2024-02-23 18:17:54 -0800

    incorperate apollo grapgql server into main server

--- server/server.js
+++ server/server.js
@@ -4,14 +4,30 @@ require('dotenv').config();
 const cors = require('cors');
 const PORT = process.env.PORT || 4000;
 const connectDB = require('./config/connection.js');
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const { typeDefs, resolvers } = require('./schema/schema.js');
 
 const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
 
 connectDB();
 
 app.use(cors()); // relax security applied to an API
-app.use('/', (req, res) => {
-    res.send('Hello World!')
-});
 
-app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+// app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+async function startServer() {
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: PORT },
+    });   
+    
+    console.log(`🚀  Server ready at: ${url}`);
+}
+
+startServer();
+
+
+  
